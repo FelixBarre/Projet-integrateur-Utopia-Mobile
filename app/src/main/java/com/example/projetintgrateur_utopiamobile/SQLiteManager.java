623c0992a60cf46ebe5f4afe0694dd34919e3626
Felix Barre <202263237@cegepsherbrooke.qc.ca>
2024-05-13 15:43:12 -0400

    Création tables DB

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/SQLiteManager.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/SQLiteManager.java
@@ -26,10 +26,20 @@ public class SQLiteManager extends SQLiteOpenHelper {
     private static final String VILLES_TABLE_NAME = "villes";
     private static final String ID_FIELD = "id";
     private static final String ID_USER_FIELD = "id_user";
+    private static final String ID_COMPTE_FIELD = "id_compte";
+    private static final String ID_DEMANDEUR_FIELD = "id_demandeur";
+    private static final String ID_ETAT_DEMANDE_FIELD = "id_etat_demande";
+    private static final String ID_TYPE_DEMANDE_FIELD = "id_type_demande";
     private static final String NOM_FIELD = "nom";
     private static final String SOLDE_FIELD = "solde";
     private static final String TAUX_INTERET_FIELD = "taux_interet";
     private static final String EST_VALIDE_FIELD = "est_valide";
+    private static final String FERME_FIELD = "ferme";
+    private static final String LIMITE_FIELD = "limite";
+    private static final String DATE_DEMANDE_FIELD = "date_demande";
+    private static final String DATE_TRAITEMENT_FIELD = "date_traitement";
+    private static final String RAISON_FIELD = "raison";
+    private static final String MONTANT_FIELD = "montant";
 
     public SQLiteManager(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -61,6 +71,57 @@ public class SQLiteManager extends SQLiteOpenHelper {
                 .append(SOLDE_FIELD)
                 .append(" DECIMAL, ")
                 .append(TAUX_INTERET_FIELD)
+                .append(" DECIMAL)");
+
+        db.execSQL(sql.toString());
+
+        sql = new StringBuilder()
+                .append("CREATE TABLE ")
+                .append(CONVERSATIONS_TABLE_NAME)
+                .append("(")
+                .append(ID_FIELD)
+                .append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
+                .append(FERME_FIELD)
+                .append(" TINYINT)");
+
+        db.execSQL(sql.toString());
+
+        sql = new StringBuilder()
+                .append("CREATE TABLE ")
+                .append(CREDITS_TABLE_NAME)
+                .append("(")
+                .append(ID_FIELD)
+                .append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
+                .append(ID_COMPTE_FIELD)
+                .append(" INTEGER, ")
+                .append(NOM_FIELD)
+                .append(" TEXT, ")
+                .append(LIMITE_FIELD)
+                .append(" DECIMAL, ")
+                .append(EST_VALIDE_FIELD)
+                .append(" TINYINT)");
+
+        db.execSQL(sql.toString());
+
+        sql = new StringBuilder()
+                .append("CREATE TABLE ")
+                .append(DEMANDES_TABLE_NAME)
+                .append("(")
+                .append(ID_FIELD)
+                .append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
+                .append(ID_DEMANDEUR_FIELD)
+                .append(" INTEGER, ")
+                .append(ID_ETAT_DEMANDE_FIELD)
+                .append(" INTEGER, ")
+                .append(ID_TYPE_DEMANDE_FIELD)
+                .append(" INTEGER, ")
+                .append(DATE_DEMANDE_FIELD)
+                .append(" DATE, ")
+                .append(DATE_TRAITEMENT_FIELD)
+                .append(" DATE, ")
+                .append(RAISON_FIELD)
+                .append(" TEXT, ")
+                .append(MONTANT_FIELD)
                 .append(" DECIMAL)");
 
         db.execSQL(sql.toString());
