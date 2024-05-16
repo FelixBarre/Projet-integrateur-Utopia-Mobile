927e491326f11d9950c1437a488a13a51321242a
jaypomaloney <mathisl525@gmail.com>
2024-05-16 10:40:21 -0400

    demandes de prêt layout

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/ComptesBancairesActivity.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/ComptesBancairesActivity.java
@@ -1,6 +1,9 @@
 package com.example.projetintgrateur_utopiamobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ComptesBancairesActivity extends AppCompatActivity {
+
+    ArrayList<CompteBancaire> comptes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,23 @@ public class ComptesBancairesActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
+        TextView test = (TextView) findViewById(R.id.titleCompte);
+        Button button = (Button) findViewById(R.id.button2);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpClient api = HttpClient.instanceOfClient();
+                    String responseGET = api.get("comptesBancaires");
+                    test.setText("responseGET");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
 }
