1adb921d17518aa0906cd22083c21cfcfdfe99ec
Max Belval-Michaud <142822452+MaxThaUnicorn@users.noreply.github.com>
2024-05-16 08:29:11 -0400

    Correction nom de fichiers drawable

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/MainActivity.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.projetintgrateur_utopiamobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -27,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnConnexion = (Button) findViewById(R.id.btnConnexion);
+        btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //http request
+
+                //store token in a local file
+                String filename = "token";
+                String fileContents = ""; //TODO: add token
+                try
+
+
+
+
+
+                Intent intent = new Intent(MainActivity.this, detailsProfil.class);
+                startActivity(intent);
+            }
         });
     }
 }
