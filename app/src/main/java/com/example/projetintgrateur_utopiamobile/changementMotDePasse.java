cb8d21496e9abcb304124cc40d61d4a5710c2c40
Max Belval-Michaud <142822452+MaxThaUnicorn@users.noreply.github.com>
2024-05-19 11:56:45 -0400

    Fonctionnalité changement de mot de passe

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/changementMotDePasse.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/changementMotDePasse.java
@@ -1,12 +1,18 @@
 package com.example.projetintgrateur_utopiamobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONObject;
 
 public class changementMotDePasse extends AppCompatActivity {
 
@@ -19,6 +25,48 @@ public class changementMotDePasse extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText inputOldPassword = (EditText) findViewById(R.id.oldPasswordInput);
+        EditText inputNewPassword = (EditText) findViewById(R.id.newPasswordInput);
+        EditText inputConfirmPassword = (EditText) findViewById(R.id.confirmNewPasswordInput);
+        TextView outputError = (TextView) findViewById(R.id.erreursOutput);
+        Button btnSubmit = (Button) findViewById(R.id.soumettreButton);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputError.setText("");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            HttpClient httpClient = HttpClient.instanceOfClient();
+                            String responsePUT = httpClient.put("passwordApi", "{ \"current_password:api\" : \"" + inputOldPassword.getText() + "\", \"password\" : \"" + inputNewPassword.getText() + "\", \"password_confirmation\" : \"" + inputConfirmPassword.getText() + "\" }");
+                            JSONObject response = new JSONObject(responsePUT);
+                            JSONObject erreur = null;
+
+                            if (response.has("ERREUR")) {
+                                if (response.get("ERREUR").getClass() == JSONObject.class) {
+                                    erreur = new JSONObject(response.get("ERREUR").toString());
+                                    for (int i = 0; i < erreur.length(); i++) {
+                                        String erreurString = erreur.getString(erreur.names().get(i).toString());
+                                        String correctedErreurString = erreurString.replaceAll("[]\"\\[]", "") + "\n";
+                                        outputError.append(correctedErreurString);
+                                    }
+                                } else {
+                                    outputError.setText(response.get("ERREUR").toString());
+                                }
+                            } else if (response.has("SUCCÈS")) {
+                                outputError.setTextColor(getColor(R.color.green));
+                                outputError.setText(getString(R.string.SuccèsConfirmationMDP));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
         });
     }
 }
