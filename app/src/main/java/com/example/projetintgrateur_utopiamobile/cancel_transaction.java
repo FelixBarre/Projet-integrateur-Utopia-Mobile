09605d4d6af5dc5c9bd514308aef4470e4fa647a
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-21 16:23:11 -0400

    MAJ des methodes ajout et annulation des transactions

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/cancel_transaction.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/cancel_transaction.java
@@ -1,6 +1,8 @@
 package com.example.projetintgrateur_utopiamobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,11 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class cancel_transaction extends AppCompatActivity {
+public class cancel_transaction extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnAnnuler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,9 @@ public class cancel_transaction extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnAnnuler = (Button) findViewById(R.id.cancelTransaction);
+        btnAnnuler.setOnClickListener(this);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -40,4 +51,13 @@ public class cancel_transaction extends AppCompatActivity {
         date.setText(currentDate);
         descriptionTransaction.setText(typeTransaction+ " de "+ montantTransaction + " vers "+ destinataireTransaction);
     }
+
+    public void onClick(View v){
+        if(v.getId()==R.id.valideTransaction) {
+
+
+        }
+    }
+
+
 }
