14782e8eef6332878e875bfeaeec7d3305d15035
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-22 13:15:38 -0400

    Afficahe du message d'erreur pour les transactions

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/show_transactions.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/show_transactions.java
@@ -2,6 +2,7 @@ package com.example.projetintgrateur_utopiamobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -46,32 +47,59 @@ public class show_transactions extends AppCompatActivity {
                     HttpClient httpClient = HttpClient.instanceOfClient();
                     String responseGET = httpClient.get("transactionsApiAll/"+idCompte);
                     JSONObject Json = new JSONObject(responseGET);
-                    JSONArray arrayJson = Json.getJSONArray("data");
 
-                    for (int i = 0; i < arrayJson.length(); i++) {
-                        JSONObject objJson = arrayJson.getJSONObject(i);
-                        TransactionClass  transaction = new TransactionClass();
-                        transaction.setId(objJson.getInt("id"));
-                        transaction.setMontant(objJson.getDouble("montant"));
-                        transaction.setCompteEnvoyeur(objJson.getString("id_compte_envoyeur"));
-                        transaction.setCompteReceveur(objJson.getString("id_compte_receveur"));
-                        transaction.setTypeTransaction(objJson.getString("id_type_transaction"));
-                        transaction.setEtatTransaction(objJson.getString("id_etat_transaction"));
-                        transaction.setDateTransaction(objJson.getString("created_at"));
-                        transaction.setDateTransactionModifie(objJson.getString("updated_at"));
-                        transactions.add(transaction);
+                    if (Json.has("data") && Json.getJSONArray("data").length() > 0) {
+                        JSONArray arrayJson = Json.getJSONArray("data");
+
+                        for (int i = 0; i < arrayJson.length(); i++) {
+                            JSONObject objJson = arrayJson.getJSONObject(i);
+                            TransactionClass  transaction = new TransactionClass();
+                            transaction.setId(objJson.getInt("id"));
+                            transaction.setMontant(objJson.getDouble("montant"));
+                            transaction.setCompteEnvoyeur(objJson.getString("id_compte_envoyeur"));
+                            transaction.setCompteReceveur(objJson.getString("id_compte_receveur"));
+                            transaction.setTypeTransaction(objJson.getString("id_type_transaction"));
+                            transaction.setEtatTransaction(objJson.getString("id_etat_transaction"));
+                            transaction.setDateTransaction(objJson.getString("created_at"));
+                            transaction.setDateTransactionModifie(objJson.getString("updated_at"));
+                            transactions.add(transaction);
+                        }
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Adapter_transaction adapterTransaction = new Adapter_transaction(show_transactions.this, transactions);
+                                recyclerViewTransaction.setAdapter(adapterTransaction);
+                                recyclerViewTransaction.setLayoutManager(new LinearLayoutManager(show_transactions.this));
+
+
+                            }
+                        });
+
+                    }else{
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        TextView messageErreur= findViewById(R.id.textError);
+                                        messageErreur.setText("Désolé, il y a aucune transaction pour ce compte");
+                                        messageErreur.setVisibility(View.VISIBLE);
+                                    }
+                                });
+
+
+                            }
+                        });
+
                     }
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Adapter_transaction adapterTransaction = new Adapter_transaction(show_transactions.this, transactions);
-                            recyclerViewTransaction.setAdapter(adapterTransaction);
-                            recyclerViewTransaction.setLayoutManager(new LinearLayoutManager(show_transactions.this));
 
 
-                        }
-                    });
 
 
                 } catch (IOException e) {
