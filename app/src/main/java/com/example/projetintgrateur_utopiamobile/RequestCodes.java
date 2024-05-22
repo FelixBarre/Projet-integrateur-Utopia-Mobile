d69f1bb1717540ceafd4fd09e167b39851a51119
Max Belval-Michaud <142822452+MaxThaUnicorn@users.noreply.github.com>
2024-05-22 12:26:27 -0400

    Ajout écran chargement changement mot de passe

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/RequestCodes.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/RequestCodes.java
@@ -5,4 +5,5 @@ public class RequestCodes {
     public static final int MAIN_ACTIVITY_REQUEST_CODE = 102;
     public static final int CAMERA_PERM_CODE = 103;
     public static final int CAMERA_REQUEST_CODE = 104;
+    public static final int CHANGEMENT_MDP_REQUEST_CODE = 105;
 }
