ab6f4c1be29e84448cf9ed6ca4838caf76f1252f
Max Belval-Michaud <142822452+MaxThaUnicorn@users.noreply.github.com>
2024-05-22 10:33:09 -0400

    Ajout connectionManager pour internet

---
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/ConnectionManager.java
@@ -0,0 +1,20 @@
+package com.example.projetintgrateur_utopiamobile;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectionManager {
+    private ConnectivityManager connectivityManager;
+
+    public ConnectionManager(Context context) {
+        connectivityManager = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
+    }
+
+    public boolean isConnected() {
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+}
