15f3464db6b9bbee3421f22e3c2b97efc6ba88c4
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-26 12:02:21 -0400

    JavaDoc completé

--- app/src/main/java/com/example/projetintgrateur_utopiamobile/TransactionStatus.java
+++ app/src/main/java/com/example/projetintgrateur_utopiamobile/TransactionStatus.java
@@ -1,3 +1,8 @@
+/****************************************
+ Fichier : TransactionStatus
+ @author : Joel Tidjane
+ Date : 2024-05-26
+ ****************************************/
 package com.example.projetintgrateur_utopiamobile;
 
 import android.content.Intent;
@@ -13,10 +18,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+/**
+ * Création de la class TransactionStatus
+ * Affiche un message de confirmation de la transaction
+ */
 public class TransactionStatus extends AppCompatActivity implements View.OnClickListener{
 
+    /**
+     * Déclaration de l'objet button pour retourner à l'activité main
+     */
     private Button btnRetour;
 
+    /**
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     *     Création de l'activité TransactionStatus
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +48,10 @@ public class TransactionStatus extends AppCompatActivity implements View.OnClick
             return insets;
         });
 
+        /**
+         * Initialisation des objets
+         */
+
         TextView titleTransaction = (TextView) findViewById(R.id.titleTransaction);
         ImageView imgSucces = (ImageView) findViewById(R.id.imgSucces);
         TextView titleStatusTransaction = (TextView) findViewById(R.id.titleStatusTransaction);
@@ -37,8 +61,16 @@ public class TransactionStatus extends AppCompatActivity implements View.OnClick
 
     }
 
+    /**
+     *
+     * @param v The view that was clicked.
+     *
+     */
     @Override
     public void onClick(View v) {
+        /**
+         * Retour à l'activité principale accueil
+         */
         if (v.getId()==R.id.valideTransaction){
             Intent intent = new Intent(TransactionStatus.this, accueil.class);
             startActivity(intent);
